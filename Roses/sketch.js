74c5a5271d30664fc0af0a44a37f0bf67bb71795
CCYHD <cheungyunhassall@hotmail.com>
2020-09-05 12:06:23 +1000

    Added 3D to roses

--- Roses/sketch.js
+++ Roses/sketch.js
@@ -1,95 +1,129 @@
-
 const config = {
-
-  backgroundColour: '#0f0f0f',
+  backgroundColour: "#0f0f0f",
   redrawBackground: true,
-  uiColour: 'rgba(0, 100, 220, 1.0)',
+  uiColour: "rgba(0, 100, 220, 1.0)",
   hideUI: false,
 
   numericVariable: 50,
   booleanVariable: false,
-  
+
   reactToAudio: false,
 
-  n: 7,
+  n: 10,
   d: 49,
 
   showRose: false,
+  showMaurerRose: true,
+  zScale: 1,
+  rotate: false,
+  popOut: false,
 };
 
 const ui = [];
 
 function setup() {
-
-  createCanvas(windowWidth, windowHeight);
+  // createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight, WEBGL);
   textSize(18);
   background(config.backgroundColour);
 
-
-  ui.push(new Slider('n', 1, 30, 1));
-  ui.push(new Slider('d', 1, 360, 1));
-  ui.push(new Checkbox('showRose'));
+  ui.push(new Slider("n", 1, 30, 1));
+  ui.push(new Slider("d", 1, 360, 1));
+  ui.push(new Slider("zScale", 0, 1, 0.01));
+  ui.push(new Checkbox("showRose"));
+  ui.push(new Checkbox("showMaurerRose"));
+  ui.push(new Checkbox("rotate"));
+  ui.push(new Checkbox("popOut"));
 
   if (config.hideUI) {
-    ui.forEach( elem => elem.hide() );
+    ui.forEach((elem) => elem.hide());
   }
 
 }
-
 
 let scale = 200;
 let points = 10000;
 
+
+let offset = 0;
 function draw() {
   if (config.redrawBackground) {
-    resizeCanvas(windowWidth, windowHeight);
+    if (width != windowWidth || height != windowHeight) {
+      resizeCanvas(windowWidth, windowHeight);
+    }
     background(config.backgroundColour);
   }
 
-
-  ui.forEach(elem => elem.update());
+  ui.forEach((elem) => elem.update());
 
   if (!config.hideUI) {
-    ui.forEach(elem => elem.draw());
+    ui.forEach((elem) => elem.draw());
   }
+
+  orbitControl();
+
 
   // CONTENTS
-  translate(width/2, height/2);
+  // translate(width / 2, height / 2);
   angleMode(DEGREES);
   noFill();
-  
-  
-  // Maurer Rose
-  stroke(255);
-  beginShape();
-  for (let i = 0; i < 361; i++) {
-    let k = i * config.d;
-    let r = scale * sin(config.n * k);
-    let angle = k;
-    let {x, y} = p2c(r, angle);
-    vertex(x, y);
-  }
-  endShape();
 
-  // Rose
-  if (config.showRose) {
-    stroke(0, 255, 0, 255);
-    let maxTheta = 180 * config.d;
+
+  if (config.rotate) {
+    offset += 0.5;
+  }
+  // rotateX(45);
+  rotateY(offset);
+
+  if (config.popOut) {
+    translate(0, 0, scale/2);
+  }
+
+  // Maurer Rose
+  if (config.showMaurerRose) {
+
+    stroke(255, 255, 255, 100);
+    strokeWeight(1);
     beginShape();
-    for (let i = 0; i < points; i++) {
-      let k = config.n / config.d;
-      let theta = maxTheta * i / points;
-      let r = scale * sin(config.n * theta);
-      let {x, y} = p2c(r, theta);
-      vertex(x, y);
+    for (let i = 0; i < 361; i++) {
+      let k = i * config.d;
+      let r = scale * sin(config.n * k);
+      let angle = k;
+      let { x, y } = p2c(r, angle);
+      let z = config.zScale;
+      if (config.popOut) {
+        z *= (1 - Math.abs(r));
+      } else {
+        z *= r;
+      }
+      vertex(x, y, z);
     }
     endShape();
   }
 
+  // Rose
+  if (config.showRose) {
+    stroke(0, 0, 255, 255);
+    strokeWeight(3);
+    beginShape();
+    for (let i = 0; i < 361; i++) {
+      let k = i;
+      let r = scale * sin(config.n * k);
+      let angle = k;
+      let { x, y } = p2c(r, angle);
+      let z = config.zScale;
+      if (config.popOut) {
+        z *= (1 - Math.abs(r));
+      } else {
+        z *= r;
+      }
+      vertex(x, y, z);
+    }
+    endShape();
+  }
 }
-
 
 const p2c = (r, angle) => ({
   x: r * cos(angle),
-  y: r * sin(angle)
+  y: r * sin(angle),
 });
