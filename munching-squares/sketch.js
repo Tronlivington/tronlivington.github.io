0bbcaf1f89c697f4a46aeee59d4829317559c356
CCYHD <cheungyunhassall@hotmail.com>
2020-09-05 17:51:51 +1000

    Changed spacing and colours

--- munching-squares/sketch.js
+++ munching-squares/sketch.js
@@ -1,65 +1,67 @@
 const config = {
   // bgColour: '#ffffff',
-  bgColour: '#000000',
-  // width: 1024,
-  width: 1200,
+  bgColour: "#000000",
+  bgRedraw: false,
+  width: 1030,
+  onStroke: 0,
+  offStroke: 50,
+  strokeWeight: 1,
+};
+
+const params = {
+  n: 1,
+  speed: 0.01,
   boxes: 64,
   spacing: 5,
 };
 
-const params = {
-  n: 2
-};
+let t;
+let prevStates = [];
+
 
 function setup() {
   createCanvas(config.width, config.width);
   background(config.bgColour);
+  t = 3*PI/2;
 }
 
-let n = 0;
-let increasing = true;
-let prevStates = [];
 function draw() {
+  if (config.bgRedraw) background(config.bgColour);
 
-  // background(config.bgColour);
+  params.n = floor(((params.boxes + 1) * (sin(t) + 1)) / 2);
+  t += params.speed;
 
-  if (params.n >= config.boxes) increasing = false;
-  if (params.n <= 0) increasing = true;
-  params.n += increasing ? 1 : -1;
+  let boxSize = floor(config.width / params.boxes);
 
-  let boxSize = floor(config.width / config.boxes);
-
-  noStroke();
-  // stroke(100);
-  let hue = map(params.n, 0, config.boxes, 50, 360);
+  strokeWeight(config.strokeWeight);
+  let hue = map(params.n, 0, params.boxes, 50, 360);
   colorMode(HSB);
 
-  for (let i = 0; i < config.boxes; i++) {
-
-    for (let j = 0; j < config.boxes; j++) {
+  for (let i = 0; i < params.boxes; i++) {
+    for (let j = 0; j < params.boxes; j++) {
       let state = false;
-      if ( parseInt(i ^ j) < params.n ) {
-        state = true
+      if (parseInt(i ^ j) < params.n) {
+        state = true;
       }
-      let prevState = prevStates[i + j * config.boxes];
+      let prevState = prevStates[i + j * params.boxes];
       if (prevState === undefined || prevState != state) {
         if (state) {
           fill(hue, 255, 255);
+          stroke(config.onStroke);
         } else {
           // fill(220, 0, 0);
           fill(0, 0, 0);
+          stroke(config.offStroke);
         }
         // fill(colour);
         rect(
-        // ellipse(
-          i * boxSize,
-          j * boxSize,
-          boxSize - config.spacing
+          // ellipse(
+          i * boxSize + params.spacing,
+          j * boxSize + params.spacing,
+          boxSize - params.spacing
         );
       }
-      prevStates[i + j * config.boxes] = state;
+      prevStates[i + j * params.boxes] = state;
     }
-
   }
-
 }
